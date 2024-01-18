b8af5e43bff121431dc05bcd57272c4177266caf
Renato Crovella <94860809+RenatoCrovella@users.noreply.github.com>
2024-01-18 13:57:45 -0300

    Update main.py
    Addded with app.app_context():
        db.create_all()

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 db = SQLAlchemy()
 db.init_app(app)
 
+with app.app_context():
+    db.create_all() 
 
 # CONFIGURE TABLES
 class BlogPost(db.Model):
